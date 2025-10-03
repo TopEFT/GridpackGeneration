61a55d66850521ba8155656f2ce82bf7ebc8799a
Brent Yates <yates.313@osu.edu>
2025-10-03 15:06:42 -0400

    Fix names

--- mcgeneration/setup_production.sh
+++ mcgeneration/setup_production.sh
@@ -8,7 +8,7 @@ GENPRODPATH=${EFTMCPATH}/../../genproductions
 if [ "$(hostname)" == "cmscon.hep.wisc.edu" ]; then
     # path for cmsconnect submit node
     GENPRODPATH="/home/${USER}/genproductions_run3"/genproductions
-elif [ "$(hostname)" == "galdos_condor.crc.nd.edu" ]; then
+elif [ "$(hostname)" == "condor.crc.nd.edu" ]; then
     GENPRODPATH="/scratch365/${USER}"/genproductions
 fi
 ### end of settings 
@@ -27,13 +27,13 @@ else
     # Checkout the repo so that it corresponds to some fixed point in development
     git checkout bc6b1f8d5
     # Copy relevant code  
-    for FILE in Utilities patches addons helpers scanfiles gridpack_generation.sh diagram_generation.sh clean_diagrams.sh submit_madpack_ttbareft.sh configure_gridpack.py transfer_gridpacks.py transfer_diagrams.py submit_cmsconnect_gridpack_generation.sh submit_galdos_condor_gridpack_generation.sh ; do 
+    for FILE in Utilities patches addons helpers scanfiles gridpack_generation.sh diagram_generation.sh clean_diagrams.sh submit_madpack_ttbareft.sh configure_gridpack.py transfer_gridpacks.py transfer_diagrams.py submit_cmsconnect_gridpack_generation.sh submit_glados_condor_gridpack_generation.sh ; do 
     #for FILE in addons helpers scanfiles diagram_generation.sh clean_diagrams.sh submit_madpack_ttbareft.sh configure_gridpack.py transfer_gridpacks.py transfer_diagrams.py ; do 
 	cp -r ${EFTMCPATH}/${FILE} ${GENPRODPATH}/bin/MadGraph5_aMCatNLO/.
     done
     cp -r ${EFTMCPATH}/PLUGIN ${GENPRODPATH}/bin/MadGraph5_aMCatNLO/.
     chmod 755 ${GENPRODPATH}/bin/MadGraph5_aMCatNLO/submit_cmsconnect_gridpack_generation.sh
-    chmod 755 ${GENPRODPATH}/bin/MadGraph5_aMCatNLO/submit_galdos_condor_gridpack_generation.sh
+    chmod 755 ${GENPRODPATH}/bin/MadGraph5_aMCatNLO/submit_glados_condor_gridpack_generation.sh
     cd ${GENPRODPATH}/.
     echo "You are done with setting up genproduction for EFT gridpack generation!"
 fi                           
