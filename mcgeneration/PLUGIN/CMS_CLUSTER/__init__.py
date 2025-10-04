b833bee77d7e55debd43535a09817a7e9905f525
Brent Yates <yates.313@osu.edu>
2025-10-04 16:53:39 -0500

    Fix singularity issue

--- mcgeneration/PLUGIN/CMS_CLUSTER/__init__.py
+++ mcgeneration/PLUGIN/CMS_CLUSTER/__init__.py
@@ -59,7 +59,7 @@ def singularityWraper():
     if "lxplus" in hostname:
         return f'\nMY.WantOS = \"{hostname.split(".")[0].replace("lxplus","el")}\"\n' # Following https://batchdocs.web.cern.ch/local/submit.html#os-selection-via-containers and simply using hostname 
     elif "uscms" in hostname:
-        return '\nMY.WantOS = \"el9\"\n' # Following https://batchdocs.web.cern.ch/local/submit.html#os-selection-via-containers and simply using hostname 
+        return '\nMY.WantOS = \"scl7\"\n' # Following https://batchdocs.web.cern.ch/local/submit.html#os-selection-via-containers and simply using hostname 
 
 def cleansubproc(subproc):
     subproc.terminate()
@@ -234,7 +234,6 @@ class CMSCondorCluster(CondorCluster):
                   getenv=True
                   %(leave_in_queue)s
                   Requirements = HAS_SINGULARITY == True
-                  +SingularityImage = "/cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el9:latest"
                   
                   +JobFlavour = "%(job_flavour)s"
                   
