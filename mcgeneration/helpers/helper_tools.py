3e932593be4dd545c09b86eb0de1039e903921ba
Brent Yates <yates.313@osu.edu>
2025-10-05 07:45:27 -0500

    Fix byte decode

--- mcgeneration/helpers/helper_tools.py
+++ mcgeneration/helpers/helper_tools.py
@@ -11,7 +11,7 @@ def run_process(inputs,verbose=True,indent=0):
     p = subprocess.Popen(inputs,stdout=subprocess.PIPE)
     stdout = []
     while True:
-        l = p.stdout.readline().decode()
+        l = p.stdout.readline().decode('utf-8')
         if l == '' and p.poll() is not None:
             break
         if l:
