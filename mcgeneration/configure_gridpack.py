3ce3a3b35d2929831bbc1c57d0c21542b46995d0
Brent Yates <yates.313@osu.edu>
2025-10-22 15:30:44 -0500

    Remove restrict

--- mcgeneration/configure_gridpack.py
+++ mcgeneration/configure_gridpack.py
@@ -4,7 +4,7 @@ import shutil
 import random
 import time
 
-from helpers.helper_tools import linspace, parse_limit_file, find_process, make_restrict_card
+from helpers.helper_tools import linspace, parse_limit_file, find_process
 from helpers.ScanType import ScanType
 from helpers.BatchType import BatchType
 from helpers.DegreeOfFreedom import DegreeOfFreedom
@@ -299,39 +299,6 @@ def submit_1dim_jobs(gp,dofs,npts,runs,tag_postfix='',max_submits=-1,run_wl={}):
         else:
             low_lim,high_lim = gp.getOption('default_limits')
 
-        ###############################
-        restrict_ops = gp.getOption('restrict')
-        if restrict_ops:
-            # The getModel() method takes care of the case where we've specified a different model than
-            #   what the MGProcess process card uses
-            model = gp.getModel()
-            model_dir = "addons/models/{model}".format(model=model)
-            ref_restrict = restrict_ops['ref']
-            # We can't use str.removesuffix since it isn't available in python3.6
-            if ref_restrict.endswith('.dat'):
-                new_restrict = "{ref}_{name}".format(ref=ref_restrict.replace('.dat',''),name=dof.getName())
-            # The model import syntax in MG expects a specific naming convention -> MODEL-massless_NAME,
-            #   where _NAME corresponds to the restrict .dat card and would look like: restrict_massless_NAME.dat
-            new_model = "{model}-massless_{restrict}".format(model=model,restrict=dof.getName())
-            new_restrict = new_restrict + ".dat"
-
-            ref_restrict = os.path.join(model_dir,ref_restrict)
-            new_restrict = os.path.join(model_dir,new_restrict)
-            # Create a new restrict card in the directory of the model specified by the MGProcess
-            keep = restrict_ops['keep']
-            # Its ok to specify a parameter that doesn't show up in a particular lhablock. Also,
-            #   for operators that have been defined as linear combinations, we need to use the actual
-            #   parameter names, rather than what we get from dof.getName()
-            blocks = {k: list(dof.getCoefficients()) for k in restrict_ops['blocks']}
-            make_restrict_card(ref_restrict,new_restrict,keep=keep,**blocks)
-            # We can't use the 'replace_model' option to set the restrict card as this will overwrite
-            #   that option, which might have already been used to specify a different model. Modifying
-            #   the gridpack options like this feels rather dangerous, but not sure how to do this
-            #   any other way
-            restrict_ops['replace'] = [model,new_model]
-            gp.setOptions(restrict=restrict_ops)
-        ###############################
-
         for idx,start in enumerate(linspace(low_lim,high_lim,runs)):
             if dof_name in run_wl and idx not in run_wl[dof_name]:
                 continue
@@ -522,7 +489,6 @@ def main():
     gridpack.setOptions(runcard_ops=rc_ops)
     # For using a different model
     gridpack.setOptions(coupling_string="FCNC=0 DIM6=1",replace_model=["SMEFTsim_topU3l_MwScheme_UFO", "dim6top_LO_UFO_19-05-20"])
-    gridpack.setOptions(restrict=restrict)
     # For creating feynman diagrams
     #gridpack.setOptions(btype=BatchType.LOCAL,save_diagrams=True,replace_model="dim6top_LO_UFO_each_coupling_order_v2020-05-19")
     #gridpack.setOptions(coupling_string="FCNC=0 DIM6^2=1 DIM6_ctB^2=1 DIM6_ctW^2=1") # For example
